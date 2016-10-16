2fd3e6f7ab653e59825812b1248308cd9c3d58c5
Stela <Stela@DESKTOP-KFO5BUG>
2016-10-16 14:18:20 +0300

    HomeworkThirdLesson

---
+++ src/HomeworkThirdLesson.java
@@ -0,0 +1,15 @@
+
+public class HomeworkThirdLesson {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int sum = x + y;
+		int z = 3;
+        System.out.print("(x + y) * z = ");
+        System.out.println(sum + z);
+
+        
+	}
+
+}
